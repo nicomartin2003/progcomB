c090a6aa17f4ccdf1f5ac1c4f28e40b3203f7101
nicomartin2003 <nikomartin2003@gmail.com>
2023-08-17 08:45:08 -0300

    Primer commit

---
+++ Programacion 1/Tp2 - Programacion 1.py
@@ -0,0 +1,42 @@
+# Ejercicios 1
+numero1 = 50
+# Ejercicio 2
+numero2 = 5.5
+# Ejercicio 3
+suma = numero1 + numero2
+# Ejercicio 4
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+# Ejercicio 5
+nombre = "Nicolas"
+# Ejercicio 6
+precio = 100
+# Ejercicio 7
+descuento = 0.20
+# Ejercicio 8
+precios_final = (precio * descuento)
+# Ejercicio 9
+cadena = "Hola Mundo"
+# Ejercicio 10
+longitud = len(cadena)
+# Ejercicio 11
+precio = int(550.7)
+# Ejercicio 12
+nombre = "Nicolas "
+apellido = "Martin "
+nombre_completo = nombre + apellido
+# Ejercicio 13
+edad = 20 + 5 - 10
+# Ejercicio 14
+altura = 1.76 * 4 / 3
+# Ejercicio 15
+nombre_mayus = " NICOLAS "
+nombre_mayus = nombre_mayus.lower
+print(nombre_mayus)
+# Ejercicio 16
+nombre_mayus = " NICOLAS "
+nombre_minus = nombre_mayus.title()
+print(f"Nombre en mayusculas: {nombre_minus}")
+print(f"----------------------")
+print(f"suma: {suma}, resta: {resta}, multiplicacion: {multiplicacion}, division: {division}")
